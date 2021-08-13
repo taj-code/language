066c78a646f6e1bc26f661b2a5a873398783084c
taj-code <tajofisher4@gmail.com>
2021-08-13 14:57:25 -0700

    fixed scripts that was not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function) {
+$(document).ready(function() {
   $("#lang").submit(function(event) {
     event.preventDefult();
     const userNameInput = $("input#userName").val();
