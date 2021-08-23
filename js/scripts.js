055020f6c4e9d037a96189b3de87857591e8de2b
taj-code <tajofisher4@gmail.com>
2021-08-22 22:22:24 -0700

    adding in radio/button

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
     const userNameInput = $("input#userName").val();
     const birthdayInput = $("input#birthday").val();
     const colorInput = $("input#color").val();
+    const favMusicInput = $("input#favMusic").val();
 
      $(".userName").append(userNameInput);
      $(".birthday").append(birthdayInput);
      $(".color").append(colorInput);
+     $(".favMusic").append(favMucicInput);
 
 
-    
       $("#final").show();
 
   });
