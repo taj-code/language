024849adfe273924fa494bb54c52728da805dd40
taj-code <tajofisher4@gmail.com>
2021-08-23 01:42:41 -0700

    add content and new bug

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,6 @@ $(document).ready(function() {
     const colorInput = $("input#color").val();
     const musicInput = $("input:radio[name=music]:checked").val();
     const subjectInput = $("#subject").val();
-        
-    if (musicInput = "Rock") {
-        $("#c").show();
-        $("span#answer").text("c")
 
      $(".userName").append(userNameInput);
      $(".birthday").append(birthdayInput);
@@ -17,15 +13,13 @@ $(document).ready(function() {
      $(".music").append(musicInput);
      $(".subject").append(subjectInput);
 
-    //  if (music >= "rock") {
-    //   $("#c").show();
-    //   $("span#final").text("c")
-    //  }
+      if (musicInput="rock") {
+        $("#c").show();
+        $("#final").show();
+      }
 
       $("#final").show();
 
-      
-      }
   });
 });
 
