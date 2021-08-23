1e126b725b888890a95053351758d9e34ae0a1a9
taj-code <tajofisher4@gmail.com>
2021-08-22 20:29:36 -0700

    new question and content

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   $("#lang").submit(function(event) {
     event.preventDefault();
     const userNameInput = $("input#userName").val();
-    
-    $(".userName").append(userNameInput);
+    const birthdayInput = $("input#birthday").val();
+
+     $(".userName").append(userNameInput);
+     $(".birthday").append(birthdayInput);
 
 
     
-    $("#final").show();
+      $("#final").show();
 
   });
 });
