a0764e90ce55e7e1f02167009a427b6156b3a47f
taj-code <tajofisher4@gmail.com>
2021-08-23 22:31:04 -0700

    clean/run

--- js/scripts.js
+++ js/scripts.js
@@ -6,34 +6,18 @@ $(document).ready(function() {
     const colorInput = $("input#color").val();
     const musicInput = $("input:radio[name=music]:checked").val();
     const subjectInput = $("#subject").val();
-    // const subjectInput = parseInt($("subjectInput#subject").val());
+
      $("#c").hide();
      $(".userName").append(userNameInput);
      $(".birthday").append(birthdayInput);
      $(".color").append(colorInput);
      $(".music").append(musicInput);
      $(".subject").append(subjectInput);
-    // if (musicInput === rock) {
-    //   $("#final").show();
-    //   $("span#type").text("c")
-    // }
-    //  $(".subject").append(subjectInput);
  
     if (subjectInput === "art") {
         $("#final").show();
         $("#c").show();
         $("span#type").text("c")
       }
-
-  // comment below out
-  // $("#final").show();
-
   });
 });
-
-
-
-// if (musicInput="rock") {
-//   $("#c").show();
-//   $("#final").show();
-// }
